f0eb171f8ef365b88f4b12dc4ba35c3bc5cd7fd4
EC2 Default User <ec2-user@ip-10-0-0-147.ec2.internal>
2020-05-15 08:13:59 +0000

    added userdata to my ec2.tf file

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,21 @@
+#creating an instance to host a  webserver running on http  
+
 resource "aws_instance" "example" {
- ami		= "ami-0323c3dd2da7fb37d"
- instance_type	= "t2.micro"
- security_groups    = ["mysecgrp"]
- key_name           = "myvision"
+ 	ami		= "ami-0323c3dd2da7fb37d"
+ 	count= 1
+ 	instance_type	= "t2.micro"
+ 	security_groups    = ["mysecgrp"]
+ 	key_name           = "myvision"
+ 	user_data = <<-EOF
+		#! /bin/bash
+		sudo yum install httpd -y
+	 	sudo systemctl start httpd
+	 	sudo systemctl enable httpd
+	 	#echo "<hi>Sample Creating Webserver Using Terraform<br>myvision</hi>" >> /var/www/html/index.html
+ 
+	EOF
+
   tags = {
-    Name            = "terra"
-  }
+   	  Name = "webserver"
+    }
 }
